28d2872bba329f78fa4bdd594dff13cc5fe9b4aa
Kundan Kumar Sahu <espkundan@gmail.com>
2023-02-10 18:46:31 +0530

    added api

--- app.js
+++ app.js
@@ -21,7 +21,42 @@ app.post('/', (req, res) => {
   const lastName = req.body.LastName;
   const email = req.body.Email;
   console.log(email);
-})
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: 'subscribed',
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = 'https://us21.api.mailchimp.com/3.0/lists/d41d7d529d';
+  
+  const options = {
+    method: 'POST',
+    auth: 'flash:4b9cbbc191737500ac8b8adc53601bab-us21'
+  }
+
+  const request = https.request(url, options, (response) => {
+    response.on('data', (data) => {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+
+});
+
+//API-KEY = 4b9cbbc191737500ac8b8adc53601bab-us21
+// Audience ID = d41d7d529d
 
 
 app.listen(port, () => {
